5766911ea1860f0f452d5d389dcccdeaa6d30d94
Vadimatorik <deryabkinda@mail.ru>
2017-07-19 15:22:34 +0700

    Применен новый вид интерфейса SPI.

--- module_shift_register.h
+++ module_shift_register.h
@@ -6,11 +6,11 @@
 
 // SPI и GPIO должен быть инициализирован заранее.
 struct module_shift_register_cfg_t {
-    const pin*  const st;                   // Вывод, подключенный к защелке.
-    const uint32_t    number_output_byte;         // Колличество байт для вывода. Если 0 - вывод не используется.
-          uint8_t*    const buffer_out;
-    const bool        strob_active;               // Состояния вывода разрешающего сигнал защелки.
-          spi_base*   const p_spi;
+    const pin*                    const st;                   // Вывод, подключенный к защелке.
+    const uint32_t                number_output_byte;         // Колличество байт для вывода. Если 0 - вывод не используется.
+          uint8_t*                const buffer_out;
+    const bool                    strob_active;               // Состояния вывода разрешающего сигнал защелки.
+          spi_master_8bit_base*   const p_spi;
 };
 
 class module_shift_register {
