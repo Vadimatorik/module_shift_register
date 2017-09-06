40df37887e778b2a12a6f7636dc8fe00eda89f50
Vadimatorik <deryabkinda@mail.ru>
2017-09-06 12:01:00 +0700

    Пользователь в методе инициализации передает указатель на буфер.

--- module_shift_register.cpp
+++ module_shift_register.cpp
@@ -1,7 +1,9 @@
 #include "module_shift_register.h"
 
-void module_shift_register::init ( void ) const {
+void module_shift_register::init ( uint8_t* data_array_reg ) const {
     this->cfg->st->set( !this->cfg->strob_active );              // Переводим защелку в неактивное положение.
+    if ( data_array_reg == nullptr ) while ( true );
+    this->data_array_reg = data_array_reg;
 }
 
 // Перезаписать все на выходах.
@@ -9,7 +11,7 @@ void module_shift_register::write ( void ) const {
     if ( this->cfg->mutex != nullptr)
         USER_OS_TAKE_MUTEX( *this->cfg->mutex, portMAX_DELAY );   // Ждем, пока освободится SPI.
 
-    this->cfg->p_spi->tx( this->cfg->buffer_out, this->cfg->number_output_byte, 10 );
+    this->cfg->p_spi->tx( this->data_array_reg, this->cfg->number_output_byte, 10 );
 
     // Защелкой делаем обновление.
     this->cfg->st->set( this->cfg->strob_active );
--- module_shift_register.h
+++ module_shift_register.h
@@ -8,7 +8,6 @@
 struct module_shift_register_cfg_t {
     const pin_base*               const st;                   // Вывод, подключенный к защелке.
     const uint32_t                number_output_byte;         // Колличество байт для вывода. Если 0 - вывод не используется.
-          uint8_t*                const buffer_out;
     const bool                    strob_active;               // Состояния вывода разрешающего сигнал защелки.
           spi_master_8bit_base*   const p_spi;
           USER_OS_STATIC_MUTEX*   const mutex;                // Может быть не указан (nullptr).
@@ -16,10 +15,11 @@ struct module_shift_register_cfg_t {
 
 class module_shift_register {
 public:
-    constexpr module_shift_register( const module_shift_register_cfg_t* const cfg ) : cfg(cfg) {}
-    void init    ( void ) const;
+    constexpr module_shift_register ( const module_shift_register_cfg_t* const cfg ) : cfg(cfg) {}
+    void init    ( uint8_t* data_array_reg ) const;
     void write   ( void ) const;
 
 private:
     const module_shift_register_cfg_t* const cfg;
+    mutable     uint8_t*             data_array_reg = nullptr;
 };
