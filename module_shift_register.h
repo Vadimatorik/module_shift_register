4a43380d4bbd452aca9150755d3e17992a9a82e4
Vadim Deryabkin <deryabkinda@mail.ru>
2017-10-25 20:05:08 +0700

    Fix m.

--- module_shift_register.h
+++ module_shift_register.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "mk_hardware_interfaces_pin.h"
-#include "mk_hardware_interfaces_spi.h"
+#include "mc_hardware_interfaces_pin.h"
+#include "mc_hardware_interfaces_spi.h"
 #include "user_os.h"
 
 // SPI и GPIO должен быть инициализирован заранее.
