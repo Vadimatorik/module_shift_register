19cce928e061253809d9beb379bb1ed43b536f56
Vadimatorik <Deryabkinda@mail.ru>
2018-03-10 20:45:35 +0700

    Привязка к буферу для передачи теперь на этапе компиляции.

--- module_shift_register.cpp
+++ module_shift_register.cpp
@@ -1,16 +1,15 @@
 #include "module_shift_register.h"
 
-void module_shift_register::init ( uint8_t* data_array_reg ) const {
-    this->cfg->st->set( !this->cfg->strob_active );					// Переводим защелку в неактивное положение.
-    this->data_array_reg = data_array_reg;
+void module_shift_register::init ( void ) const {
+	this->cfg->st->set( !this->cfg->strob_active );					// Переводим защелку в неактивное положение.
 }
 
 // Перезаписать все на выходах.
 void module_shift_register::write ( void ) const {
-    if ( this->cfg->mutex != nullptr)
+	if ( this->cfg->mutex != nullptr )
         USER_OS_TAKE_MUTEX( *this->cfg->mutex, portMAX_DELAY );
 
-    this->cfg->p_spi->tx( this->data_array_reg, this->cfg->number_output_byte, 10 );
+	this->cfg->p_spi->tx( this->cfg->data_array, this->cfg->array_size, 10 );
 
     // Защелкой делаем обновление.
     this->cfg->st->set( this->cfg->strob_active );
--- module_shift_register.h
+++ module_shift_register.h
@@ -6,20 +6,20 @@
 
 // SPI и GPIO должен быть инициализирован заранее.
 struct module_shift_register_cfg_t {
-    const pin_base*               const st;                   // Вывод, подключенный к защелке.
-    const uint32_t                number_output_byte;         // Колличество байт для вывода. Если 0 - вывод не используется.
-    const bool                    strob_active;               // Состояния вывода разрешающего сигнал защелки.
-          spi_master_8bit_base*   const p_spi;
-          USER_OS_STATIC_MUTEX*   const mutex;                // Может быть не указан (nullptr).
+	const pin_base*					const st;			// Вывод, подключенный к защелке.
+	const uint8_t*					data_array;			// Внутренний буфер.
+	const uint32_t					array_size;			// Размер буфера.
+	const bool						strob_active;		// Состояния вывода разрешающего сигнал защелки.
+	const spi_master_8bit_base*		const p_spi;		// Используемый SPI.
+	const USER_OS_STATIC_MUTEX*		const mutex;		// Может быть не указан (nullptr).
 };
 
 class module_shift_register {
 public:
-    constexpr module_shift_register ( const module_shift_register_cfg_t* const cfg ) : cfg(cfg) {}
-    void init    ( uint8_t* data_array_reg ) const;
-    void write   ( void ) const;
+	constexpr module_shift_register ( const module_shift_register_cfg_t* const cfg ) : cfg( cfg ) {}
+	void init	( void ) const;
+	void write	( void ) const;
 
 private:
-    const module_shift_register_cfg_t* const cfg;
-    mutable     uint8_t*             data_array_reg = nullptr;
+	const module_shift_register_cfg_t*		const cfg;
 };
