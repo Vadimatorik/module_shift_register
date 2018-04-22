542d28d2380a1d964544197bfcd0567c32e6ee24
Vadimatorik <deryabkinda@mail.ru>
2018-04-22 11:01:11 +0700

    Новые аппаратные интерфейсы (связь).

---
+++ shift_register.cpp
@@ -0,0 +1,20 @@
+#include "shift_register.h"
+
+void ShiftRegister::init ( void ) {
+	this->cfg->st->set( !this->cfg->strobActive );					// Переводим защелку в неактивное положение.
+}
+
+// Перезаписать все на выходах.
+void ShiftRegister::write ( void ) {
+	if ( this->cfg->mutex != nullptr )
+        USER_OS_TAKE_MUTEX( *this->cfg->mutex, portMAX_DELAY );
+
+	this->cfg->spiObj->tx( this->cfg->dataArray, this->cfg->arraySize, 10 );
+
+    // Защелкой делаем обновление.
+    this->cfg->st->set( this->cfg->strobActive );
+    this->cfg->st->set( !this->cfg->strobActive );
+
+    if ( this->cfg->mutex != nullptr)
+        USER_OS_GIVE_MUTEX( *this->cfg->mutex );					// Разрешаем использование SPI другим потокам.
+}
---
+++ shift_register.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "mc_hardware_interfaces_pin.h"
+#include "mc_hardware_interfaces_spi.h"
+#include "user_os.h"
+
+// SPI и GPIO должен быть инициализирован заранее.
+struct ShiftRegisterCfg {
+	PinBase*						const st;			// Вывод, подключенный к защелке.
+	const uint8_t*					dataArray;			// Внутренний буфер.
+	const uint32_t					arraySize;			// Размер буфера.
+	const bool						strobActive;		// Состояния вывода разрешающего сигнал защелки.
+	SpiMaster8BitBase*				const spiObj;		// Используемый SPI.
+	USER_OS_STATIC_MUTEX*			const mutex;		// Может быть не указан (nullptr).
+};
+
+class ShiftRegister {
+public:
+	constexpr ShiftRegister ( const ShiftRegisterCfg* const cfg ) : cfg( cfg ) {}
+
+	void init	( void );
+	void write	( void );
+
+private:
+	const ShiftRegisterCfg*		const cfg;
+};
